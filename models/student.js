654e7d49a3f86e25a4bce8e81f5b2c5b517890d6
Chi Huynh <chi@skyhawk-277gt.local>
2019-08-23 14:31:56 -0700

    create Student model

---
+++ models/student.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const studentSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        avatar: String,
+        email: String,
+        googleId: String
+    },
+    timestamps: true
+});
+
+
+
+
+module.exports = mongoose.model('Student', studentSchema);
